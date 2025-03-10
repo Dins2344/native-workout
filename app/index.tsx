6fadd85d412d3347e126a122e5c51ec1196e20af
Dins2344 <dinson.cd@gmail.com>
2025-03-11 00:34:58 +0530

    added modal component

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,80 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Button, Modal, Text, View, StyleSheet } from "react-native";
+import {
+  GestureHandlerRootView,
+  ScrollView,
+} from "react-native-gesture-handler";
 
 export default function Index() {
+  const [isOpen, setIsOpen] = useState(false);
+  const txtCount = [1, 2, 3, 4, 5, 6, 7, 8, 910, 11, 12, 13, 14, 15,16,17,18,19];
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor : 'red'
-      }}
-    >
-      <Text>Hello World again</Text>
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          backgroundColor: "white",
+        }}
+      >
+        <Text>Hello World</Text>
+        <Button title="Click Me" onPress={() => setIsOpen(true)}></Button>
+
+        <Modal
+          transparent={true}
+          visible={isOpen}
+          animationType="slide"
+          onRequestClose={() => setIsOpen(false)}
+        >
+          <View style={styles.modalContainer}>
+            <View style={styles.modalDiv}>
+              <ScrollView style={{ display: "flex", flexDirection: "column" }}>
+              {txtCount.map((count) => (
+                <Text key={count}>Hi Iam open {count}</Text>
+              ))}
+            </ScrollView>
+              {/* <ScrollView style={{ display: "flex", flexDirection: "column" }}>
+                <Text>Hi Iam open</Text>
+                <Text>Hi Iam open</Text>
+                <Text>Hi Iam open</Text>
+                <Text>Hi Iam open</Text>
+                <Text>Hi Iam open</Text>
+                <Text>Hi Iam open</Text>
+                <Text>Hi Iam open</Text>
+                <Text>Hi Iam open</Text>
+                <Text>Hi Iam open</Text>
+              </ScrollView> */}
+              <Button
+                title="Click Me"
+                onPress={() => setIsOpen(false)}
+              ></Button>
+            </View>
+          </View>
+        </Modal>
+      </View>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  modalDiv: {
+    width: "100%",
+    height: "50%",
+    // margin: 50,
+    padding: 30,
+    backgroundColor: "white",
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: -3,
+    },
+    shadowOpacity: 0.55,
+    elevation: 5,
+  },
+});
